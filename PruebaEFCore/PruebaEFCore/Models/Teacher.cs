56648a81ff9900410c25ad14912ab5b3ece9b04c
Alicia Lemos <alemos@leafnoise.io>
2022-10-20 20:56:37 -0300

    Proyecto con DatabaseFirst

---
+++ PruebaEFCore/PruebaEFCore/Models/Teacher.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PruebaEFCore.Models
+{
+    public partial class Teacher
+    {
+        public Teacher()
+        {
+            Course = new HashSet<Course>();
+        }
+
+        public int TeacherId { get; set; }
+        public string TeacherName { get; set; }
+        public int? StandardId { get; set; }
+
+        public virtual Standard Standard { get; set; }
+        public virtual ICollection<Course> Course { get; set; }
+    }
+}
